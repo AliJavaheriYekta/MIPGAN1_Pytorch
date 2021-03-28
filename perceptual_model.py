a11b92510867d74a9df9837b588c857a269beb28
Ali Javaheri <a.javaheri1373@gmail.com>
2021-03-28 14:07:48 +0430

    Update perceptual_model.py

--- perceptual_model.py
+++ perceptual_model.py
@@ -4,40 +4,28 @@ import torch.nn as nn
 
 
 class VGG16_for_Perceptual(torch.nn.Module):
-    def __init__(self,requires_grad=False,n_layers=[2,4,9,14,21]):
+    def __init__(self,requires_grad=False,n_layers):
         super(VGG16_for_Perceptual,self).__init__()
         vgg_pretrained_features=models.vgg16(pretrained=True).features 
 
-        self.slice0=torch.nn.Sequential()
-        self.slice1=torch.nn.Sequential()
-        self.slice2=torch.nn.Sequential()
-        self.slice3=torch.nn.Sequential()
-        self.slice4=torch.nn.Sequential()
-
-        for x in range(n_layers[0]):#relu1_1
-            self.slice0.add_module(str(x),vgg_pretrained_features[x])
-        for x in range(n_layers[0],n_layers[1]): #relu1_2
-            self.slice1.add_module(str(x),vgg_pretrained_features[x])
-        for x in range(n_layers[1],n_layers[2]): #relu3_2
-            self.slice2.add_module(str(x),vgg_pretrained_features[x])
-        for x in range(n_layers[2],n_layers[3]):#relu4_2
-            self.slice3.add_module(str(x),vgg_pretrained_features[x])
-        for x in range(n_layers[3],n_layers[4]):#relu4_2
-            self.slice3.add_module(str(x),vgg_pretrained_features[x])
+        self.slices = []
+        for i in n_layers:
+            slice = torch.nn.Sequential()
+            for j in range(i):
+                slice.add_module(str(j),vgg_pretrained_features[j])
+            self.slices.append(slice)
         
         if not requires_grad:
             for param in self.parameters():
                 param.requires_grad=False
         
-
+        self.results = []
     
     def forward(self,x):
-        h0=self.slice0(x)
-        h1=self.slice1(h0)
-        h2=self.slice2(h1)
-        h3=self.slice3(h2)
-        h4=self.slice4(h3)
+        for slice in self.slices:
+            x = slice(x)
+            self.results.append(x)
 
-        return h0,h1,h2,h3,h4
+        return self.results
 
 
