35b15f9c4a1d8648372d264cc6c859be6c7a7a0d
Ali Javaheri <a.javaheri1373@gmail.com>
2021-03-28 20:38:23 +0430

    Update perceptual_model.py

--- perceptual_model.py
+++ perceptual_model.py
@@ -1,12 +1,11 @@
 import torch 
-from torchvision import models 
-import torch.nn as nn 
+from torchvision import models
 
 
 class VGG16_for_Perceptual(torch.nn.Module):
     def __init__(self,requires_grad=False,n_layers=None,device='cpu'):
         super(VGG16_for_Perceptual,self).__init__()
-        vgg_pretrained_features=models.vgg16(pretrained=True).features.to(device)
+        vgg_pretrained_features=models.vgg16(pretrained=True).features
 
         self.slices = []
         pre_layer = 0
@@ -16,18 +15,15 @@ class VGG16_for_Perceptual(torch.nn.Module):
                 slice.add_module(str(j),vgg_pretrained_features[j])
             self.slices.append(slice)
             pre_layer = i
-        
+        self.results = []
         if not requires_grad:
             for param in self.parameters():
-                param.requires_grad=False
-        
-        self.results = []
-    
+                param.requires_grad=False       
+        self.device = device
     def forward(self,x):
+        self.results = []
+        x = x.to(self.device)
         for slice in self.slices:
             x = slice(x)
             self.results.append(x)
-
-        return self.results     
-
-
+        return self.results      
